7bb12f3f486b7263b75e8b79dfba436e575a634d
Sultannis <sultan.fb29@gmail.com>
2021-04-26 08:39:08 +0600

    feat: add styles to header component

--- src/App.vue
+++ src/App.vue
@@ -10,4 +10,18 @@ export default {};
 
 <style lang="scss">
 @import "@/assets/styles/global.scss";
+@font-face {
+  font-family: "San-Francisco";
+  src: local("Geometria Thin"), local("Geometria-Thin"),
+    url("~@/assets/fonts/san-francisco/sf_ui_display_regular.woff")
+      format("woff"),
+    url("~@/assets/fonts/san-francisco/sf_ui_display_regular.woff2")
+      format("woff2");
+  font-weight: normal;
+  font-style: normal;
+}
+
+#app {
+  font-family: San-Francisco;
+}
 </style>
--- src/components/common/halykHeader.vue
+++ src/components/common/halykHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="header">
     <img src="@/assets/icons/logo.svg" alt="" class="header__logo" />
-    <input type="text" class="header__input" />
+    <input type="text" class="header__input" placeholder="Поиск" />
     <div class="header__controls">
       <img src="@/assets/icons/calendar.svg" alt="" class="header__icon-link" />
       <img src="@/assets/icons/bell.svg" alt="" class="header__icon-link" />
       <div class="header__profile">
-        <img src="" alt="" class="header__avatar" />
+        <img src="@/assets/img/avatar.jpg" alt="" class="header__avatar" />
         <div class="header__name">Daniyer Egeubay</div>
       </div>
     </div>
@@ -18,24 +18,55 @@ export default {};
 </script>
 
 <style lang="scss">
+@import "@/assets/styles/variables.scss";
+
 .header {
   height: 70px;
   width: 100%;
 
   display: flex;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
 
   &__logo {
-    width: 15%;
+    width: 100px;
+    height: 60%;
   }
 
   &__input {
-    width: 60%;
+    height: 60%;
+    padding-left: 10px;
+    text-align: left;
+    flex: 1;
+
+    border: 1px solid $primary;
+    border-radius: 5px;
   }
 
   &__controls {
-    width: 25%;
-
     display: flex;
+    margin: 0 30px;
+    align-items: center;
+  }
+
+  &__icon-link {
+    margin: 0 10px;
+  }
+
+  &__avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-right: 20px;
+  }
+
+  &__profile {
+    display: flex;
+    align-items: center;
+  }
+
+  &__name {
+    color: rgb(78, 78, 78);
   }
 }
 </style>
